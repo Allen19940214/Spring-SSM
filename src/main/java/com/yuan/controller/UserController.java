b45661b5d920b8a9bb63513867d10ce3bcd16997
jiangyuan <ilzyy1421@163.com>
2021-12-23 11:53:08 +0800

    修改首页 修改接口

--- src/main/java/com/yuan/controller/UserController.java
+++ src/main/java/com/yuan/controller/UserController.java
@@ -5,20 +5,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yuan.pojo.User;
 import com.yuan.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     @Autowired
     UserService userService;
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("msg","helloSSM");
+        return "hello";
+    }
+
     @RequestMapping("/user/findAll")
+    @ResponseBody
     public String findAll() throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         List<User> all = userService.findAll();
         return objectMapper.writeValueAsString(all);
-
     }
 }
